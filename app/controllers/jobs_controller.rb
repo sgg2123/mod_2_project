b5eaab5af1c4bc5776ce9cf15665a68d90a3ea95
sgg2123 <sgg2123@columbia.edu>
2018-05-30 17:28:11 -0400

    integrated API based job search

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,21 +1,28 @@
 class JobsController < ApplicationController
     before_action :get_job, only: [:show, :edit, :update, :destroy]
 
-    def index
-    end
+    # def index
+    # end
+    #
+    # def new
+    #   @job = Job.new
+    #   @languages = Language.all
+    # end
+    #
+    # def create
+    #   @job = Job.create(job_params)
+    #   redirect_to @job
+    # end
 
-    def new
+    def search
       @job = Job.new
-      @languages = Language.all
     end
 
-    def create
-      @job = Job.create(job_params)
-      redirect_to @job
+    def results
+      language = Language.find(job_params["language_id"]).lang_name
+      @job_results = Job.job_search(language)
     end
 
-    def show
-    end
 
     def edit
     end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,39 @@
+require 'pry'
+require 'rest-client'
+require 'json'
+
 class Job < ActiveRecord::Base
   belongs_to :language
   has_many :user_jobs
   has_many :users, through: :user_jobs
+
+  def self.make_request(url)
+    all_characters = RestClient.get(url)
+    JSON.parse(all_characters)
+  end
+
+
+  #######
+  def self.job_search(pg_num = 1, language)
+    url = "https://api.adzuna.com:443/v1/api/jobs/us/search/#{pg_num}?app_id=189edd09&app_key=1feefbefc8cdd067528cf11b48d913fd&what=#{language}&what_and=developer&where=10010&distance=25&max_days_old=120&full_time=1"
+
+    data = make_request(url)
+
+    listing_array = []
+
+    data["results"].each do |listing|
+      listing_hash = {company_name: "", title: "", description: ""}
+      listing_hash[:company_name] = listing["company"]["display_name"].gsub("<strong>", "").gsub("</strong>", "")
+      listing_hash[:title] = listing["title"].gsub("<strong>", "").gsub("</strong>", "")
+      listing_hash[:description] = listing["description"].gsub("<strong>", "").gsub("</strong>", "")
+      listing_hash[:redirect_url] = listing["redirect_url"]
+      listing_hash[:id] = listing["id"]
+      listing_array << listing_hash
+    end
+  listing_array
+  end
+  # until pg_num >= data["count"]/10.0 + 1
+  #   pg_num += 1
+  # end
+
 end
