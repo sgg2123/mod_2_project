247095c889a1a0320ae8c6dd0dd16ae7a4d19f7d
github username <jeffreyhwang90@gmail.com>
2018-05-31 11:31:27 -0400

    new job route

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,16 @@ Rails.application.routes.draw do
 
   resources :users
   resources :languages
-  resources :jobs, except: [:create, :new, :show]
+  resources :jobs, except: [:index, :create, :new, :show]
 
   root 'users#welcome', as: "welcome"
 
-   get "signup", to: "users#new", as: "signup"
-   get "login", to: "sessions#new", as: "login"
-   post "sessions", to: "sessions#create", as: "sessions"
-   post "logout", to: "sessions#destroy", as: "logout"
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  post "sessions", to: "sessions#create", as: "sessions"
+  post "logout", to: "sessions#destroy", as: "logout"
 
-   get "jobs/search", to: "jobs#search", as: "search"
-   post "jobs", to: "jobs#results", as: "results"
-   # get "jobs/results", to: "jobs#results", as: "results"
-   # post "jobs/results", to: "jobs#results", as: "post_results"
+  get "jobs/search", to: "jobs#search", as: "search"
+  get "jobs", to: "jobs#results", as: "results"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2018_05_30_201014) do
 
-  create_table "companies", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "company_languages", force: :cascade do |t|
-    t.integer "company_id"
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.string "company_name"
+    t.text "description"
+    t.string "redirect_url"
+    t.integer "listing_id"
     t.integer "language_id"
   end
 
@@ -35,9 +35,9 @@ ActiveRecord::Schema.define(version: 2018_05_30_201014) do
     t.string "school"
   end
 
-  create_table "user_companies", force: :cascade do |t|
+  create_table "user_jobs", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "company_id"
+    t.integer "job_id"
   end
 
   create_table "user_languages", force: :cascade do |t|
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(version: 2018_05_30_201014) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password_digest"
     t.string "username"
+    t.string "password_digest"
   end
 
 end
